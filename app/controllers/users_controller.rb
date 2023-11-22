afe84b1c0eeb444725e57ebb6e8390f5744a06b4
Takahiro Egawa <2111140085a@ed.fuk.kindai.ac.jp>
2023-11-22 08:31:45 +0000

    ログイン機能の設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,43 @@
+require 'bcrypt'
+
 class UsersController < ApplicationController
+  def index
+    if session[:login_username] != nil
+      render "logout"
+    else
+      render "index"
+    end
+  end
+  
   def login
+    user = User.find_by(username: params[:username])
+    
+    if user && BCrypt::Password.new(user.password) == params[:password]
+      session[:login_username] = params[:username]
+      redirect_to users_complete_path
+    #else
+      #render "error"
+    end
   end
 
   def new
+    # render 'new'
+  end
+  
+  def make
+    make_pass = BCrypt::Password.create(params[:password])
+    user = User.new(username: params[:username], password: make_pass)
+    user.save
+    session[:login_username] = params[:username]
+    redirect_to users_complete_path
   end
 
   def complete
+    #render "complete"
+  end
+  
+  def logout
+    session.delete(:login_username)
+    redirect_to users_login_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_061221) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
