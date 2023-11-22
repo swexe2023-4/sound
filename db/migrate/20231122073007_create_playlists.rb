337a68e7c61b9218cd9a3c357a2ae7f680c03e8e
2111140081h <2111140081h@ed.fuk.kindai.ac.jp>
2023-11-22 07:32:35 +0000

    Created Playlist model

---
+++ db/migrate/20231122073007_create_playlists.rb
@@ -0,0 +1,11 @@
+class CreatePlaylists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :playlists do |t|
+      t.string :playlistname
+      t.integer :user_id
+      t.integer :song_id
+
+      t.timestamps
+    end
+  end
+end
