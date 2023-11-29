43102113ba51ba146860b5598e82d560a184d1ab
Takahiro Egawa <2111140085a@ed.fuk.kindai.ac.jp>
2023-11-29 07:09:14 +0000

    ログイン機能の完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,30 @@
 Rails.application.routes.draw do
-  get 'users/logout'
-  post 'users/login'
-  root "users#index"
+  # root "users#index"
+  # post 'users/login'
   
+  
+  # get 'users/login'
+  # get 'users/logout'
+  # get 'users/new'
+  # get 'users/complete'
+  # post 'users/login'
+
+  # post 'users/make', to: 'users#make'
+  
+  # resources :users
+  
+  root "users#logout"  
+  
+  post 'users/login'
   get 'users/login'
+  get 'users/logout'
   get 'users/new'
   get 'users/complete'
-  post 'users/login'
+
+  post 'users/make', to: 'users#make'
   
   resources :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
